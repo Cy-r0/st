e97a22bc8e0ebc2f1ef5bb244701455d3a94918a
Ciro <ciro@ciro-n551.wind3.hub>
2023-05-01 19:14:47 +0200

    enable bell sound

--- config.h
+++ config.h
@@ -72,7 +72,7 @@ static unsigned int cursorthickness = 2;
  * bell volume. It must be a value between -100 and 100. Use 0 for disabling
  * it
  */
-static int bellvolume = 0;
+static int bellvolume = 100;
 
 /* default TERM value */
 char *termname = "st-256color";
