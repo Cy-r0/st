32fc131e1839a60a03d631377a03e5f3d4e8b308
Ciro <ciro@ciro-n551.wind3.hub>
2023-05-01 18:17:23 +0200

    Apply anygeometry patch, centers term text with awkward sizes

--- config.h
+++ config.h
@@ -172,12 +172,26 @@ static unsigned int defaultrcs = 256;
  */
 static unsigned int cursorstyle = 1;
 static Rune stcursor = 0x2603; /* snowman ("â˜ƒ") */
+
+/*
+ * Whether to use pixel geometry or cell geometry
+ */
+
+static Geometry geometry = PixelGeometry;
+
 /*
  * Default columns and rows numbers
  */
 
 static unsigned int cols = 80;
 static unsigned int rows = 24;
+
+/*
+ * Default width and height (including borders!)
+ */
+
+static unsigned int width = 564;
+static unsigned int height = 364;
 
 /*
  * Default colour and shape of the mouse cursor
