ae377df1c47bdef03fc0fb8adca123a0f65f4349
Ciro <ciro@ciro-n551.wind3.hub>
2023-05-01 14:12:41 +0200

    Apply scrollback patch (no ring buffer) + reflow

--- config.h
+++ config.h
@@ -145,7 +145,8 @@ static const char *colorname[] = {
 
 	/* more colors can be added after 255 to use with DefaultXX */
         /* terminal background */
-	"#282828",
+	/* "#282828", */
+	"#280000",
 };
 
 /*
@@ -224,6 +225,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
+	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
 };
 
 /*
